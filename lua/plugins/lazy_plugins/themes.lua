ab88e9b200c09fd2b51c9ce8353dcea7bfa13332
root <dani6laviv@gmail.com>
2024-05-26 16:43:12 +0300

    github theme

--- lua/plugins/lazy_plugins/themes.lua
+++ lua/plugins/lazy_plugins/themes.lua
@@ -2,10 +2,21 @@ return {
     {
         "folke/tokyonight.nvim",
         name = "tokyonight",
-        enabled = true,
+        enabled = false,
         config = function()
             vim.cmd.colorscheme 'tokyonight-night'
             vim.cmd.hi 'Comment gui=none'
+        end,
+    },
+    {
+        "projekt0n/github-nvim-theme",
+        name = "github-nvim-theme",
+        lazy = false,
+        priority = 1000,
+        enabled = true,
+        config = function()
+            require("github-theme").setup({})
+            vim.cmd.colorscheme "github_dark_dimmed"
         end,
     },
 }

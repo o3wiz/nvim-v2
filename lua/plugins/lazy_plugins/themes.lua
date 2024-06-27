3a7a16f4bb5e7a14e30492afaadf1ddf0b7a5516
Daniel Aviv <dani6laviv@gmail.com>
2024-06-27 19:26:01 +0300

    gruvbox theme

--- lua/plugins/lazy_plugins/themes.lua
+++ lua/plugins/lazy_plugins/themes.lua
@@ -11,12 +11,19 @@ return {
     {
         "projekt0n/github-nvim-theme",
         name = "github-nvim-theme",
-        lazy = false,
-        priority = 1000,
-        enabled = true,
+        enabled = false,
         config = function()
             require("github-theme").setup({})
             vim.cmd.colorscheme "github_dark_dimmed"
         end,
     },
+    {
+        "ellisonleao/gruvbox.nvim",
+        name = "gruvbox",
+        enabled = true,
+        config = function()
+            vim.o.background = "dark"
+            vim.cmd.colorscheme 'gruvbox'
+        end,
+    }
 }
